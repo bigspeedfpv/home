323ac20ccf41676184cc3ee638860fa240f430f3
Andrew B <bigspeedfpv@gmail.com>
2023-04-16 15:57:40 -0400

    fix: move all spotify functions to one file

--- src/routes/api/spotify/np/+server.ts
+++ src/routes/api/spotify/np/+server.ts
@@ -1,4 +1,39 @@
-import { getNowPlaying, type Artist } from '$lib/server/spotify';
+import {
+	SPOTIFY_CLIENT_ID,
+	SPOTIFY_CLIENT_SECRET,
+	SPOTIFY_REFRESH_TOKEN
+} from '$env/static/private';
+
+const basic = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
+
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+
+const getAccessToken = async () => {
+	const response = await fetch(TOKEN_ENDPOINT, {
+		method: 'POST',
+		headers: {
+			Authorization: `Basic ${basic}`,
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: new URLSearchParams({
+			grant_type: 'refresh_token',
+			refresh_token: SPOTIFY_REFRESH_TOKEN
+		}).toString()
+	});
+
+	return response.json();
+};
+
+const getNowPlaying = async () => {
+	const { access_token } = await getAccessToken();
+
+	return fetch(NOW_PLAYING_ENDPOINT, {
+		headers: {
+			Authorization: `Bearer ${access_token}`
+		}
+	});
+};
 
 export async function GET() {
 	const response = await getNowPlaying();
@@ -50,3 +85,14 @@ export async function GET() {
 		}
 	);
 }
+
+type Artist = {
+	external_urls: {
+		spotify: string;
+	};
+	href: string;
+	id: string;
+	name: string;
+	type: string;
+	uri: string;
+};
