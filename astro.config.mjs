0b734fc91a42e00d770cf771fc40625ce63c7799
Andrew Brower <bigspeedfpv@gmail.com>
2025-09-30 23:32:55 -0400

    feat: mdx homepage, minor now playing style adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 import solidJs from "@astrojs/solid-js";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
-  integrations: [solidJs()],
+  integrations: [solidJs(), mdx()],
 });
--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -42,6 +42,9 @@ const NowPlaying: Component = () => {
 
 		const artistLinks = s.artists.map(artistLink);
 
+		if (artistLinks.length === 2)
+			return [artistLinks[0], " and ", artistLinks[1]];
+
 		return artistLinks.reduce(
 			(acc: JSXElement[], artist: JSXElement, idx: number) => {
 				acc.push(artist);
@@ -56,20 +59,27 @@ const NowPlaying: Component = () => {
 	return (
 		<Show when={status()}>
 			{(status) => (
-				<div class="relative top-4 md:absolute md:top-0 md:right-0 border-[1px] border-black/20 p-2 rounded-2xl flex flex-row gap-4 align-center items-center w-64">
+				<div
+					class={`
+						relative mb-4 -left-2 md:left-auto md:absolute md:top-0 md:right-0 border-[1px] border-black/20
+						p-2 rounded-2xl flex flex-row gap-4 align-center items-center w-max max-w-full md:max-w-82 h-16 md:shadow-md
+					`}
+				>
 					<img
 						src={status().images[0].url}
 						alt="Album cover"
-						class="w-12 h-12 rounded-lg"
+						class="w-12 h-12 rounded-lg border-[1px] border-black/10"
 					/>
-					<div class="flex flex-col">
-						<span class="text-xs opacity-50">Listening to</span>
-						<span class="font-bold">
+					<div class="flex flex-col gap-0 mr-2">
+						<span class="text-xs opacity-50 line-clamp-1 overflow-ellipsis">
+							Listening to
+						</span>
+						<span class="font-bold leading-4 overflow-ellipsis line-clamp-1 text-sm">
 							<a target="_blank" href={status().url}>
 								{status().name}
 							</a>
 						</span>
-						<span class="text-xs">by {artistLine()}</span>
+						<span class="text-xs line-clamp-2">by {artistLine()}</span>
 					</div>
 				</div>
 			)}
